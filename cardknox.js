f8d140fb9efe642b0e320dd2618838c4d2653f2e
Moshe Ekstein <mekstein@fidelitypayment.com>
2024-11-24 14:32:18 -0500

    Add scripts  Cardknox

--- cardknox.js
+++ cardknox.js
@@ -1,6 +1,6 @@
 // Since the API key and Cardknox interaction are handled on the server, we no longer need to call Cardknox directly from the client-side.
 export function sendPaymentToServer(paymentData) {
-    return fetch('/process-payment', {
+    return fetch('https://bigcommerce-server.onrender.com/process-payment', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(paymentData),
