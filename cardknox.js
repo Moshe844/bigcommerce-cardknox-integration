fc7fa9132d52f5c845d69dfab1c068215578fd25
Moshe844 <100170084+Moshe844@users.noreply.github.com>
2024-11-24 14:11:41 -0500

    Add files via upload

---
+++ cardknox.js
@@ -0,0 +1,8 @@
+// Since the API key and Cardknox interaction are handled on the server, we no longer need to call Cardknox directly from the client-side.
+export function sendPaymentToServer(paymentData) {
+    return fetch('/process-payment', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(paymentData),
+    }).then((response) => response.json());
+}
